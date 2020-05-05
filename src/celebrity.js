c2f5103cc2ff184a48e01a5b6c7d3bcc89b8fc67
Egg Saito <ariafloat@users.noreply.github.com>
2020-05-05 15:55:02 +0900

    Add number of celebrity passed away

--- src/celebrity.js
+++ src/celebrity.js
@@ -4,17 +4,24 @@ async function main() {
   const resCelebrity = await comm.get('data/celebrity.json');
   const celebrityList = resCelebrity.data;
 
+  let passAwayTotal = 0;
   let dom = '';
   celebrityList.forEach((val) => {
     const infectionConfirmDate = val.infectionConfirmDate.split('/');
     const birthday = val.birthday.split('/');
-    const td1 = `<td>${val.name}</td>`;
-    const td2 = `<td>${val.occupation}</td>`;
-    const td3 = `<td>${Number(infectionConfirmDate[0])}/${Number(infectionConfirmDate[1])}/${Number(infectionConfirmDate[2])}</td>`;
-    const td4 = `<td>${Number(birthday[0])}/${Number(birthday[1])}/${Number(birthday[2])}</td>`;
+    let { name } = val;
+    if (val.passAway) {
+      passAwayTotal += 1;
+      name = `${val.name}<br>（他界）`;
+    }
+    const td1 = `<td class="align-middle">${name}</td>`;
+    const td2 = `<td class="align-middle">${val.occupation}</td>`;
+    const td3 = `<td class="align-middle">${Number(infectionConfirmDate[0])}/${Number(infectionConfirmDate[1])}/${Number(infectionConfirmDate[2])}</td>`;
+    const td4 = `<td class="align-middle">${Number(birthday[0])}/${Number(birthday[1])}/${Number(birthday[2])}</td>`;
     dom += `<tr>${td1}${td2}${td3}${td4}</tr>`;
   });
   document.getElementById('celebrityTotal').innerHTML = celebrityList.length.toLocaleString();
+  document.getElementById('celebrityPassAwayTotal').innerHTML = passAwayTotal;
   document.getElementById('celebrityList').innerHTML = dom;
 }
 
